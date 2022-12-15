ad9019257e6c81ce86de20dc57e331c876c4fe26
IagorSs <iagor.penaranda@uol.com.br>
2022-12-15 01:07:44 -0300

    feat(notifications): implement post with data validator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { PrismaService } from './prisma.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
 @Controller()
 export class AppController {
@@ -17,5 +19,17 @@ export class AppController {
   @Get('notifications')
   getNotifications() {
     return this.prismaService.notification.findMany();
+  }
+
+  @Post('notifications')
+  async createNotification(@Body() body: CreateNotificationBody) {
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content: body.content,
+        category: body.category,
+        recipientId: body.recipientId,
+      },
+    });
   }
 }
