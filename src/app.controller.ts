9da4b0eefce7e573936e6c406c96408cd2faa2e4
IagorSs <iagor.penaranda@uol.com.br>
2022-12-15 01:03:20 -0300

    feat(prisma): create and implement service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
+import { PrismaService } from './prisma.service';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('notifications')
+  getNotifications() {
+    return this.prismaService.notification.findMany();
   }
 }
