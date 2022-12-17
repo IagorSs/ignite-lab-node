ad0dce4953caabc7e56ef410f98610b5462b7b98
IagorSs <iagor.penaranda@uol.com.br>
2022-12-17 16:35:55 -0300

    feat(in-memo-notification-repo): implemented to be used in tests with notification repo

---
+++ test/repositories/in-memory-notifications.ts
@@ -0,0 +1,10 @@
+import { Notification } from 'src/app/entities/notification';
+import { NotificationsRepository } from 'src/app/repositories/notifications';
+
+export class InMemoryNotificationRepository implements NotificationsRepository {
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification) {
+    this.notifications.push(notification);
+  }
+}
