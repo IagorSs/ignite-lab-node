b921b8a90ec59699935f128a538ca1c9ef6953d6
IagorSs <iagor.penaranda@uol.com.br>
2022-12-18 15:35:45 -0300

    feat(prisma-notification-mapper): mapper to-domain

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,3 +1,4 @@
+import { Content } from '@app/entities/notification/content/content';
 import { Notification } from '@app/entities/notification/notification';
 import { Notification as DbNotification } from '@prisma/client';
 
@@ -12,5 +13,18 @@ export class PrismaNotificationMapper {
       createdAt: notification.createdAt,
       canceledAt: notification.canceledAt ?? null,
     };
+  }
+
+  static toDomain(dbNotification: DbNotification): Notification {
+    return new Notification(
+      {
+        category: dbNotification.category,
+        content: new Content(dbNotification.content),
+        recipientId: dbNotification.recipientId,
+        readAt: dbNotification.readAt,
+        createdAt: dbNotification.createdAt,
+      },
+      dbNotification.id,
+    );
   }
 }
