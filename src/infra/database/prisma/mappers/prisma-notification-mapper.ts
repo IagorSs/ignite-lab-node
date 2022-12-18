797dad68511898372f5044cb91b908548be514ba
IagorSs <iagor.penaranda@uol.com.br>
2022-12-18 15:35:11 -0300

    fix(prisma-notification-mapper): type and adjust mapper.toPrisma

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,14 +1,16 @@
 import { Notification } from '@app/entities/notification/notification';
+import { Notification as DbNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
-  static toPrisma(notification: Notification) {
+  static toPrisma(notification: Notification): DbNotification {
     return {
       id: notification.id,
       category: notification.category,
       content: notification.content.value,
       recipientId: notification.recipientId,
-      readAt: notification.readAt,
+      readAt: notification.readAt ?? null,
       createdAt: notification.createdAt,
+      canceledAt: notification.canceledAt ?? null,
     };
   }
 }
